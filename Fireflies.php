83914b5593a5c8047abb48387464a6e812d0c2c0
Joy Harvel <geekgirl.joy@gmail.com>
2019-10-22 16:29:07 -0700

    Update Fireflies.php

--- Fireflies.php
+++ Fireflies.php
@@ -2,7 +2,7 @@
 
 // Example Use:
 /*
-	http://localhost/MrGoodBot/Fireflies.php
+    http://localhost/MrGoodBot/Fireflies.php
 */
 
 $path = __DIR__ . DIRECTORY_SEPARATOR . 'skins' . DIRECTORY_SEPARATOR . 'Fireflies' . DIRECTORY_SEPARATOR;
@@ -11,15 +11,15 @@ $path = __DIR__ . DIRECTORY_SEPARATOR . 'skins' . DIRECTORY_SEPARATOR . 'Firefli
 $firefly_cells = glob($path . "fireflies_jar_*.png");
 
 
-// Select A Random number of Firefly layers between 1 - 3
+// Select A Random Number Of Firefly Layers Between 4 - 8
 foreach(range(1, mt_rand(4, 8)) as $key){
     $fireflies[] = array_rand($firefly_cells, 1);
 }
 
 
 // Load Selected Image Parts
-foreach($fireflies as $key=>&$cell){
-	$cell = imagecreatefrompng($firefly_cells[$fireflies[$key]]); // Load Fireflies
+foreach($fireflies as &$cell){
+    $cell = imagecreatefrompng($firefly_cells[$cell]); // Load Fireflies
 }
 
 $background = imagecreatefrompng($path . "background.png"); // Load Background
@@ -30,25 +30,25 @@ imagealphablending($background, true);
 imagesavealpha($background, true);
 
 
-// What size is the image?
+// What Size Is The Image?
 $width = imagesx($background);
 $height = imagesy($background);
 
 
-// Merge image Parts
+// Merge Image Parts
 imagecopy($background, $jar,0, 0, 0, 0, $width, $height);
 foreach($fireflies as &$cell){
-	imagecopy($background, $cell,0, 0, 0, 0, $width, $height);
+    imagecopy($background, $cell,0, 0, 0, 0, $width, $height);
 }
 
 
-// Save New Face Image in the Root Skins DIR
+// Save New Face Image In The Root Skins DIR
 imagepng($background, __DIR__ . DIRECTORY_SEPARATOR . 'skins' . DIRECTORY_SEPARATOR .'current_face.png');
 
 
 #FreeTheMemory!!!
 foreach($fireflies as &$cell){
-	imagedestroy($cell);
+    imagedestroy($cell);
 }
 imagedestroy($jar);
 imagedestroy($background);
